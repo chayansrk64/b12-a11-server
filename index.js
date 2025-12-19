f867a5dad825f2c0542d33e32d100727a8fd437e
chayansrk64 <chayansrk64@gmail.com>
2025-12-19 11:25:21 +0600

    loan delete api created

--- index.js
+++ index.js
@@ -131,6 +131,13 @@ async function run() {
         res.send(result);
     })
 
+    app.delete('/loans/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await loanCollection.deleteOne(query)
+        res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
