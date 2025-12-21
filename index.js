2bcf05a1bad3e7c1b384eb59d8fbc55c01666947
chayansrk64 <chayansrk64@gmail.com>
2025-12-21 19:33:40 +0600

    myloans/:id for delete from loanApplicationCollection

--- index.js
+++ index.js
@@ -213,6 +213,14 @@ async function run() {
         res.send(result)
     })
 
+   
+    app.delete('/myloans/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await loanApplicationCollection.deleteOne(query)
+        res.send(result)
+    })
+
     app.patch('/loans/:id/show-home', async (req, res) => {
     const id = req.params.id;
     const { showHome } = req.body;
